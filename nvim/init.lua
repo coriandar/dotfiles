8a1d312796cc1b61a9362f281f644ab07f341955
coriandar <88608040+coriandar@users.noreply.github.com>
2023-08-31 19:30:21 +1200

    Decouple plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,3 @@
-require("config")
+require("config.set")
+require("config.remap")
+require("config.lazy")
---
+++ nvim/lua/plugins/dependencies.lua
@@ -0,0 +1,4 @@
+return {
+    "nvim-tree/nvim-web-devicons", -- lualine
+    "nvim-lua/plenary.nvim" -- harpoon/telescope
+}
