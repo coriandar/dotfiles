24b7bad884d792d5006358141a0ecab411bb8439
coriandar <88608040+coriandar@users.noreply.github.com>
2023-09-08 16:24:54 +1200

    Add fzf to telescope (linux)

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
     "nvim-telescope/telescope.nvim", 
     tag = "0.1.2",
+    dependencies = {
+         --{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- linux only
+    },
 
     config = function()
         local builtin = require('telescope.builtin')
@@ -8,5 +11,7 @@ return {
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+        --require("telescope").load_extension("fzf") -- linux only
     end
 }
